d6dc36892cef1cb567ba18e5bba3ec968cb469ff
Andrew Poon <90732419+andEWW@users.noreply.github.com>
2022-06-01 14:26:46 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+var x;
+var y;
+var numCircles;
+var radius;
+
 function setup() {
-  // put setup code here
+  createCanvas(800,800);
+numCircles = 8;
+radius = width/(numCircles*2);
 }
 
 function draw() {
-  // put drawing code here
+drawcircles(radius, numCircles);
+}
+
+
+
+function drawcircles(int radius, int n) {
+ n = 0;
+ x = radius;
+ y = height/2;
+  while (n < numCircles){
+  fill(255*n/numCircles);
+    circle(x, y, radius*2);
+   x += radius*2;
+   n += 1;
+  }
 }
